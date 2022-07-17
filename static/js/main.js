9c5f17a95030e3ed747b1a2970d5d97e9ff55a47
Alik Akhmetov <ali.mars.99@gmail.com>
2022-07-17 19:05:33 +0600

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line func-names
+// $(document).ready(function() {
+// $('.grade-section__slider')
+//   .addClass('owl-carousel owl-theme')
+//   .owlCarousel({
+//     loop: false,
+//     items: 1,
+//     dots: false,
+//     nav: false,
+//     margin: 10,
+//     responsive: {
+//       768: {
+//         items: 1,
+//         margin: 30,
+//       },
+//       1024: {
+//         items: 2,
+//         margin: 40,
+//       },
+//       1440: {
+//         items: 2,
+//         margin: 55,
+//       },
+//     },
+//   });
+// const src = $('#popup');
+// $('.grade__item').magnificPopup({
+//   removalDelay: 250,
+//   items: {
+//     src,
+//     type: 'inline',
+//   },
+//   callbacks: {
+//     beforeOpen() {
+//       this.st.mainClass = this.st.el.attr('data-effect');
+//     },
+//   },
+// });
+// });
+"use strict";
